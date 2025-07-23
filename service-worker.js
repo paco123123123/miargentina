68c5ee66236369bc4a8b6adfcf583dea1cfce22f
paco123123123 <pakito.halvarezz@gmail.com>
2025-07-23 19:02:50 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,22 @@
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('miarg-cache').then(function(cache) {
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('miargentina-cache-v1').then(cache => {
       return cache.addAll([
+        '/',
         '/index.html',
-        '/scripts.js',
-        '/styles.css',
-        '/config_dni.html',
-        '/dni.html',
-        '/dni_details.html',
-        '/error.html',
-        '/misDocumentos.html',
-        '/icon.png'
+        '/style.css',
+        '/app.js',
+        '/manifest.json',
+        '/icon.png',
+        '/icon-512.png'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
